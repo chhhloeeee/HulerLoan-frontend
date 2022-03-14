0a35cc25807a5aad57e5ec06643f4ab2a6e6ab29
Chloe <chloe.baker@huler.io>
2022-03-14 10:53:41 +0000

    added back link

--- loan-app/pages/category/list.js
+++ loan-app/pages/category/list.js
@@ -1,3 +1,14 @@
+import Link from 'next/link'
+
 export default function ListCategory() {
-    return <h1>Categories</h1>
+    return (
+      <>
+    <h1>Categories</h1>
+    <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </>
+    )
   }
--- loan-app/pages/index.js
+++ loan-app/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -17,10 +18,11 @@ export default function Home() {
         </h1>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <h2 className={styles.card}> Categories &rarr;
+          <Link href="/category/list">
+          <p>View and add categories to loan</p>
+          </Link>
+        </h2>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
