434b809bd4b9f7aec237c087fa51f68a73cf5be6
Chloe <chloe.baker@huler.io>
2022-03-18 13:36:55 +0000

    user form

--- loan-app/pages/user/add.js
+++ loan-app/pages/user/add.js
@@ -1,15 +1,19 @@
+import Button from "../../components/button";
+import FormElement from "../../components/form";
+import styles from "../../styles/form.module.css";
+
 export default function AddUser() {
     return (
-    <div className="App">
-          <h1 className="title">
+    <div className={styles.app}>
+          <h1 className={styles.title}>
               Create User
           </h1>
-          <div className='form'>
+          <div className={styles.form}>
             <form action="" method="post">
-            <FormElement for="name" text="Name" type="text" name="name" className="inputField" required></FormElement>
-            <FormElement for="email" text="Email" type="text" name="email" className="inputField"required></FormElement>
-            <FormElement for="admin" text="Admin?" type="checkbox" name="checkbox" className="checkbox"required></FormElement>
-            <FormElement for="password" text="Password" type="password" name="password" className="inputField"required></FormElement>
+            <FormElement for="name" text="Name" type="text" name="name" className={styles.inputField} required></FormElement>
+            <FormElement for="email" text="Email" type="text" name="email" className={styles.inputField}required></FormElement>
+            <FormElement for="admin" text="Admin?" type="checkbox" name="checkbox" className={styles.checkbox}required></FormElement>
+            <FormElement for="password" text="Password" type="password" name="password" className={styles.inputField}required></FormElement>
             <label><span> </span><input type="submit" value="Submit" /></label>
             </form>
         </div> 
