db21d728d10302f76539521b8030073dc63b5a8c
Chloe <chloe.baker@huler.io>
2022-03-18 13:40:11 +0000

    return form

--- loan-app/pages/user/add.js
+++ loan-app/pages/user/add.js
@@ -3,24 +3,50 @@ import FormElement from "../../components/form";
 import styles from "../../styles/form.module.css";
 
 export default function AddUser() {
-    return (
+  return (
     <div className={styles.app}>
-          <h1 className={styles.title}>
-              Create User
-          </h1>
-          <div className={styles.form}>
-            <form action="" method="post">
-            <FormElement for="name" text="Name" type="text" name="name" className={styles.inputField} required></FormElement>
-            <FormElement for="email" text="Email" type="text" name="email" className={styles.inputField}required></FormElement>
-            <FormElement for="admin" text="Admin?" type="checkbox" name="checkbox" className={styles.checkbox}required></FormElement>
-            <FormElement for="password" text="Password" type="password" name="password" className={styles.inputField}required></FormElement>
-            <label><span> </span><input type="submit" value="Submit" /></label>
-            </form>
-        </div> 
-        <Button
-        text="Cancel"
-        href="/home"
-      />
-        </div>
-    )
-  }
+      <h1 className={styles.title}>Create User</h1>
+      <div className={styles.form}>
+        <form action="" method="post">
+          <FormElement
+            for="name"
+            text="Name"
+            type="text"
+            name="name"
+            className={styles.inputField}
+            required
+          ></FormElement>
+          <FormElement
+            for="email"
+            text="Email"
+            type="text"
+            name="email"
+            className={styles.inputField}
+            required
+          ></FormElement>
+          <FormElement
+            for="admin"
+            text="Admin?"
+            type="checkbox"
+            name="checkbox"
+            className={styles.checkbox}
+            required
+          ></FormElement>
+          <FormElement
+            for="password"
+            text="Password"
+            type="password"
+            name="password"
+            className={styles.inputField}
+            required
+          ></FormElement>
+          <label>
+            <span> </span>
+            <input type="submit" value="Submit" />
+          </label>
+        </form>
+      </div>
+      <Button text="Cancel" href="/home" />
+    </div>
+  );
+}
