1d45b1f419d947454a126729ff8a8e1330a25849
chhhloeeee <isabelchloe2@gmail.com>
2022-03-22 10:38:37 +0000

    finished admin login

--- loan-app/components/login.js
+++ loan-app/components/login.js
@@ -1,22 +1,35 @@
-import Link from 'next/link'
-import style from '../styles/login.module.css'
+import Link from "next/link";
+import style from "../styles/login.module.css";
 
 function MyLink(props) {
-  let { href, children, ...rest } = props
+  let { href, children, ...rest } = props;
   return (
     <Link href={href}>
       <a {...rest}>{children}</a>
     </Link>
-  )
+  );
 }
 
 export default function Login() {
-    return (
-      <div className={style.appHeader}> 
+  return (
+    <div className={style.appHeader}>
       <h2 className={style.title}>HulerLoan Login</h2>
-        <input className={style.uid} type="text" placeholder="Username" ></input>
-        <input className={style.pwd} type="password" placeholder="Password" ></input>
-        <MyLink className={style.logbttn} href="/home">Login</MyLink>
-      </div>
-    );
-  };
+      <p className={style.subtitle}>Admin</p>
+      <input className={style.uid} type="text" placeholder="Username"></input>
+      <input
+        className={style.pwd}
+        type="password"
+        placeholder="Password"
+      ></input>
+      <MyLink className={style.logbttn} href="/home">
+        Login
+      </MyLink>
+      <MyLink className={style.accountBttn} href="/userlogin">
+        Switch account type?
+      </MyLink>
+      <MyLink className={style.registerBttn} href="/register">
+        Register account
+      </MyLink>
+    </div>
+  );
+}
