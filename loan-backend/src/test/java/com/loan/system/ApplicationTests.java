3ec755049184d5cc777f065774c503903177d55d
chhhloeeee <isabelchloe2@gmail.com>
2022-04-15 14:29:00 +0100

    updating names

--- loan-backend/src/test/java/com/loan/system/ApplicationTests.java
+++ loan-backend/src/test/java/com/loan/system/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dailycodebuffer.usersystem;
+package com.loan.system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserMngtSystemApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
