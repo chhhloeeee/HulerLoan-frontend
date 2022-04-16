2dec8cd5ffa45dbefa3c9c030ecc9503ea21a354
chhhloeeee <isabelchloe2@gmail.com>
2022-04-16 18:32:51 +0100

    adding api and table css

---
+++ loan-app/components/api.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import styles from '../styles/form.module.css';
+
+export const APILoader = ({url, Component, reloadWith}) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [response, setResponse] = useState(null)
+  const [error, setError] = useState(null)
+  
+  const fetchData = () => {
+      console.log(json.status)
+      fetch(url)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.status !== "ok") {
+          throw Error("Internal Server Error")
+        }
+        setResponse(json)
+      })
+      .catch((err) => setError(err))
+      .finally(() => setIsLoaded(true)); 
+      
+  }
+  useEffect(fetchData, reloadWith || [])
+
+  if (!isLoaded) {
+    return (
+        <h1 className={styles.title}>Loading...</h1>
+    )
+  } 
+  if (error) {
+    return (
+        <div id ="noDataLBL" className={styles.app}>Could not load </div>
+    )
+  }
+  return (
+      <Component data={response.data}/>
+  )
+}
