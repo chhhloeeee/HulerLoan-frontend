9bf40e115c109b5ad07f3ad4cf921bd19fa1e2b4
chhhloeeee <isabelchloe2@gmail.com>
2022-04-25 18:56:25 +0100

    changing to equipment id

--- loan-app/pages/loan/list.js
+++ loan-app/pages/loan/list.js
@@ -1,34 +1,31 @@
 import Button from "../../components/button";
-import styles from '../../styles/form.module.css';
+import styles from "../../styles/form.module.css";
 import Table from "../../components/table";
 import { APILoader } from "../../components/api";
-import { useAlert } from 'react-alert';
-import {BinIcon} from "../../components/icons"
-import {useState} from 'react';
-
+import { useAlert } from "react-alert";
+import { BinIcon } from "../../components/icons";
+import { useState } from "react";
 
 function ListLoan() {
-    return (
-      <div className={styles.app}>
+  return (
+    <div className={styles.app}>
       <h1 className={styles.title}>Loan</h1>
       <APILoader
-            url={'http://localhost:8080/api/v1/loan'}
-            Component={
-              LoanTable
-            }
-          />
+        url={"http://localhost:8080/api/v1/loan"}
+        Component={LoanTable}
+      />
       <Button text="Cancel" href="/home" />
     </div>
-    )
-  }
+  );
+}
 
-  //USerTable function displays all equipment in a table
-function LoanTable({data}){
-  console.log(data)
-  //const alert = useAlert() 
-  const [loan, setLoan] = useState(data)
-  console.log(data)
- 
+//USerTable function displays all equipment in a table
+function LoanTable({ data }) {
+  console.log(data);
+  //const alert = useAlert()
+  const [loan, setLoan] = useState(data);
+  console.log(data);
+
   let loanList = loan.sort((a, b) => {
     if (a.loanID < b.loanID) {
       return -1;
@@ -39,52 +36,62 @@ function LoanTable({data}){
     return 0;
   });
   const deleteLoanCall = async (loanID) => {
-    return await fetch('http://localhost:8080/api/v1/loan/' + loanID, {
-      method: "DELETE"
-    })
-  }
- 
+    return await fetch("http://localhost:8080/api/v1/loan/" + loanID, {
+      method: "DELETE",
+    });
+  };
+
   const deleteLoan = (loanID) => {
-    const array = [...loanList]
+    const array = [...loanList];
     for (let i = 0; i < array.length; i++) {
-        if (loanID === array[i].loanID) {
-            let promise = deleteLoanCall(loanID)
-            promise.then((response) => {
-              if (!response.ok) {
-                throw Error("Internal Server Error");
-              }
-              array.splice(i, 1)
-        
-              setLoan(array)
-              return
-            })
-            .catch((error)=>{
-              alert.show('Internal Server Error')
-              return
-            })
-        }    
+      if (loanID === array[i].loanID) {
+        let promise = deleteLoanCall(loanID);
+        promise
+          .then((response) => {
+            if (!response.ok) {
+              throw Error("Internal Server Error");
+            }
+            array.splice(i, 1);
+
+            setLoan(array);
+            return;
+          })
+          .catch((error) => {
+            alert.show("Internal Server Error");
+            return;
+          });
+      }
     }
-    return
-  }
+    return;
+  };
   return (
-  <Table
-    headers={["Loan ID", "User ID", "Category ID", "Issue Date", "Return Date", "Days Elapsed", "Active?", "Actions"]}
-    rows= {loanList.map((service)=> (
-      [
+    <Table
+      headers={[
+        "Loan ID",
+        "User ID",
+        "Equipment ID",
+        "Issue Date",
+        "Return Date",
+        "Days Elapsed",
+        "Active?",
+        "Actions",
+      ]}
+      rows={loanList.map((service) => [
         service.loanID,
         service.userID,
-        service.categoryID,
+        service.equipmentID,
         service.issueDate,
         service.returnDate,
         service.daysElapsed,
         service.active.toString(),
-        <div className='parent inline-flex-parent'>
-        <div className='child'><BinIcon onClick={() => deleteLoan(service.loanID)}/></div>
-      </div> 
-      ]
-    ))}
-  />
-  )
+        <div className="parent inline-flex-parent">
+          <div className="child">
+            <BinIcon onClick={() => deleteLoan(service.loanID)} />
+          </div>
+        </div>,
+      ])}
+    />
+  );
 }
 
 export default ListLoan;
--- loan-app/pages/return/add.js
+++ loan-app/pages/return/add.js
@@ -2,34 +2,62 @@ import Button from "../../components/button";
 import FormElement from "../../components/form";
 import styles from "../../styles/form.module.css";
 import DatePicker from "react-datepicker";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function AddReturn() {
   var start = new Date();
   start.setDate(start.getDate() - 1);
   const [startDate, setStartDate] = useState(start);
+  const [loading, setLoading] = useState(true);
+  const [userValue, setUserValue] = useState();
+  const [user, setUser] = useState([{ label: "Loading...", value: "" }]);
+
+  useEffect(() => {
+    let unmounted = false;
+    async function getUsers() {
+      const response = await fetch("http://localhost:8080/api/v1/users");
+      const body = await response.json();
+      console.log(body);
+      console.log(response);
+      if (!unmounted) {
+        setUser(
+          body.map(({ userID, name }) => ({
+            label: userID + ": " + name,
+            value: userID,
+          }))
+        );
+        setLoading(false);
+      }
+    }
+    getUsers();
+    return () => {
+      unmounted = true;
+    };
+  }, []);
   return (
     <div className={styles.app}>
       <h1 className={styles.title}>Create Return</h1>
       <div className={styles.form}>
         <form action="" method="post">
-          <FormElement
-            for="id"
-            text="User ID"
-            type="text"
-            name="id"
-            className={styles.inputField}
-            required
-          ></FormElement>
-          <FormElement
-            for="name"
-            text="Name"
-            type="text"
-            name="name"
-            className={styles.inputField}
-            required
-          ></FormElement>
+          <label for="id">
+            <span>
+              ID <span className={styles.required}>*</span>
+            </span>
+            <select
+              disabled={loading}
+              id="users"
+              className={styles.selectField}
+              value={userValue}
+              onChange={(e) => setUserValue(e.currentTarget.userValue)}
+            >
+              {user.map(({ label, value }) => (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              ))}
+            </select>
+          </label>
           <label for="item">
             <span>
               Item <span class={styles.required}>*</span>
@@ -45,7 +73,8 @@ export default function AddReturn() {
               Return Date<span className={styles.required}>*</span>
             </span>
           </label>
-          <DatePicker id={styles.date}
+          <DatePicker
+            id={styles.date}
             selected={startDate}
             name="return"
             className={styles.inputField}
