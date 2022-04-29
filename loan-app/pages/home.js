13cd4494aec2338beedfefc60ca4e3bdaa0ceda7
Chloe <chloe.baker@huler.io>
2022-04-29 14:41:46 +0100

    add tailwind and edit background size

--- loan-app/pages/home.js
+++ loan-app/pages/home.js
@@ -1,19 +1,21 @@
-import styles from '../styles/Home.module.css'
-import { DropdownMenu } from '../components/menu'
-import Image from 'next/image'
-import texture from '../images/bluefire.jpeg'
+import styles from "../styles/Home.module.css";
+import { DropdownMenu } from "../components/menu";
+import Image from "next/image";
+import texture from "../images/bluefire.jpeg";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-     
-      <DropdownMenu/>
+      <DropdownMenu />
       <main className={styles.main}>
-      <Image src={texture} width={2000} height={1020} className={styles.background}/>
-        <h1 className={styles.title}>
-          Welcome to HulerLoan!
-        </h1>
+        <Image
+          src={texture}
+          width={2150}
+          height={1030}
+          className={styles.background}
+        />
+        <h1 className={styles.title}>Welcome to HulerLoan!</h1>
       </main>
     </div>
-  )
+  );
 }
--- loan-app/tailwind.config.js
+++ loan-app/tailwind.config.js
@@ -2,9 +2,10 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{html,js}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
