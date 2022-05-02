3e08e9c58e4e574d0fc6d38ace65b0ce35880a39
chhhloeeee <isabelchloe2@gmail.com>
2022-05-02 17:04:19 +0100

    add user

--- loan-app/pages/user/add.js
+++ loan-app/pages/user/add.js
@@ -1,8 +1,40 @@
 import Button from "../../components/button";
 import FormElement from "../../components/form";
 import styles from "../../styles/form.module.css";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 export default function AddUser() {
+  const router = useRouter();
+  const [user, setUser] = useState({
+    userID: "",
+    name: "",
+    password: "",
+    admin: false,
+    email: "",
+  });
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setUser({ ...user, [event.target.name]: value });
+  };
+
+  const postUser = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/api/v1/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    });
+    console.log(response);
+    if (!response.ok) {
+      throw new Error("Internal Server Error");
+    }
+    router.push("/user/list");
+  };
+
   return (
     <div className={styles.app}>
       <h1 className={styles.title}>Create User</h1>
@@ -15,6 +47,8 @@ export default function AddUser() {
             name="name"
             className={styles.inputField}
             required
+            value={user.name}
+            onChange={(e) => handleChange(e)}
           ></FormElement>
           <FormElement
             for="email"
@@ -23,14 +57,18 @@ export default function AddUser() {
             name="email"
             className={styles.inputField}
             required
+            value={user.email}
+            onChange={(e) => handleChange(e)}
           ></FormElement>
           <FormElement
             for="admin"
             text="Admin?"
             type="checkbox"
-            name="checkbox"
+            name="admin"
             className={styles.checkbox}
             required
+            value={user.admin}
+            onChange={(e) => handleChange(e)}
           ></FormElement>
           <FormElement
             for="password"
@@ -39,10 +77,12 @@ export default function AddUser() {
             name="password"
             className={styles.inputField}
             required
+            value={user.password}
+            onChange={(e) => handleChange(e)}
           ></FormElement>
           <label>
             <span> </span>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" onClick={postUser} />
           </label>
         </form>
       </div>
